2e9b2e8e5c9ab0bd4e94b5a602e358a2f1526094
SrRito <ben_95_felipe@hotmail.com>
2021-03-18 14:05:15 -0600

    avance

--- bank-project/Form1.Designer.cs
+++ bank-project/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace bank_project
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.random = new System.Windows.Forms.Label();
+            this.testBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // random
+            // 
+            this.random.AutoSize = true;
+            this.random.Location = new System.Drawing.Point(198, 163);
+            this.random.Name = "random";
+            this.random.Size = new System.Drawing.Size(86, 13);
+            this.random.TabIndex = 0;
+            this.random.Text = "numero random: ";
+            // 
+            // testBtn
+            // 
+            this.testBtn.Location = new System.Drawing.Point(183, 188);
+            this.testBtn.Name = "testBtn";
+            this.testBtn.Size = new System.Drawing.Size(75, 23);
+            this.testBtn.TabIndex = 1;
+            this.testBtn.Text = "prueba";
+            this.testBtn.UseVisualStyleBackColor = true;
+            this.testBtn.Click += new System.EventHandler(this.testBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(691, 417);
+            this.Controls.Add(this.testBtn);
+            this.Controls.Add(this.random);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label random;
+        private System.Windows.Forms.Button testBtn;
     }
 }
 
--- bank-project/Form1.cs
+++ bank-project/Form1.cs
@@ -12,9 +12,16 @@ namespace bank_project
 {
     public partial class Form1 : Form
     {
+        Clientes cliente = new Clientes();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void testBtn_Click(object sender, EventArgs e)
+        {
+            random.Text = "numero random:";
+            random.Text = random.Text + cliente.getMovimientos();
         }
     }
 }
