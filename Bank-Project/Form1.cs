ae09200a11eaf761785d11514850882ae4b45f2e
SrRito <75587111+SrRito@users.noreply.github.com>
2021-03-18 15:57:19 -0600

    Version Animada 3.0

---
+++ Bank-Project/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace claseCajera
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cCajero caja1 = new cCajero(1);
+            cCajero caja2 = new cCajero(2);
+            cCajero caja3 = new cCajero(3);
+            cCajero caja4 = new cCajero(4);
+            cCajero caja5 = new cCajero(5);
+
+            MessageBox.Show("Creaste las cajas " + caja1.numeroCaja()+ " "+ caja2.numeroCaja() + caja3.numeroCaja() + caja4.numeroCaja() + caja5.numeroCaja());
+        }
+
+        //CLic boton definir express
+        /*
+           caja1.definirExpress();
+           caja2.definirExpress(); 
+           caja3.definirExpress(); 
+           caja4.definirExpress(); 
+           caja5.definirExpress(); 
+
+        */
+    }
+}
---
+++ Bank-Project/bank-project/bank-project/Banco.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bank_project
+{
+    public partial class Banco : Form
+    {
+        private int conteo;
+        Clientes cliente = new Clientes();
+        cajeroExpress express = new cajeroExpress();
+        cCajero caja1 = new cCajero(1);
+        cCajero caja2 = new cCajero(2);
+        cCajero caja3 = new cCajero(3);
+        cCajero caja4 = new cCajero(4);
+        cCajero caja5 = new cCajero(5);
+        int x=0;
+        public Banco()
+        {
+            InitializeComponent();
+            
+            express.ShowDialog();
+            x = express.getNumCaja();
+            switch (x)
+            {
+                case 1:
+                    caja1.definirExpress();
+                    lblCaja1.Visible = true;
+                    break;
+                case 2:
+                    caja2.definirExpress();
+                    lblCaja2.Visible = true;
+                    break;
+                case 3:
+                    caja3.definirExpress();
+                    lblCaja3.Visible = true;
+                    break;
+                case 4:
+                    caja4.definirExpress();
+                    lblCaja4.Visible = true;
+                    break;
+                case 5:
+                    caja5.definirExpress();
+                    lblCaja5.Visible = true;
+                    break;
+                default:
+                    break;
+            }
+            caja1.addMov(cliente.getMovimientos());
+        }
+
+        private void testBtn_Click(object sender, EventArgs e)
+        {
+            random.Text = "numero random:";
+            random.Text = random.Text + cliente.getMovimientos();
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            conteo++;
+            lblConteo.Text = conteo.ToString();
+        }
+
+    }
+}
